bee46b360c8ca6e5922942c8bbc5bfb62c2212a9
Yangsiyoung <ysjleader@gmail.com>
2022-04-07 23:05:42 +0900

    feature: Toby Chapter 02

---
+++ src/test/kotlin/com/ysjleader/tobyspringkotlin/chapter02/fake/repository/impl/FakeUserRepository.kt
@@ -0,0 +1,19 @@
+package com.ysjleader.tobyspringkotlin.chapter02.fake.repository.impl
+
+import com.ysjleader.tobyspringkotlin.chapter02.fake.domain.User
+import com.ysjleader.tobyspringkotlin.chapter02.fake.repository.UserRepository
+import java.lang.IllegalArgumentException
+
+class FakeUserRepository: UserRepository {
+
+    private val userMap: HashMap<String, User> = hashMapOf()
+
+    override fun save(user: User): User {
+        userMap[user.userId] = user
+        return userMap[user.userId] ?: throw IllegalArgumentException("뭥미")
+    }
+
+    override fun findById(userId: String): User {
+        return userMap[userId] ?: throw IllegalArgumentException("없음여")
+    }
+}
---
+++ src/test/kotlin/com/ysjleader/tobyspringkotlin/chapter02/v1/factory/ProfileUserServiceFactoryV2.kt
@@ -0,0 +1,28 @@
+package com.ysjleader.tobyspringkotlin.chapter02.v1.factory
+
+import com.ysjleader.tobyspringkotlin.chapter02.v1.login.RestTemplateLoginService
+import com.ysjleader.tobyspringkotlin.chapter02.v1.login.WebClientLoginService
+import com.ysjleader.tobyspringkotlin.chapter02.v1.user.UserService
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import javax.annotation.PostConstruct
+
+@Configuration
+class ProfileUserServiceFactoryV2 {
+
+    @Profile(value = ["local"])
+    @Bean("userServiceV2")
+    fun userService(): UserService {
+        return UserService(loginService = RestTemplateLoginService())
+
+    }
+
+    @Profile(value = ["!local"])
+    @Bean("userServiceV2")
+    fun prodUserService(): UserService {
+        return UserService(loginService = WebClientLoginService())
+    }
+
+}
